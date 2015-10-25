2c34462740e86923e1734bd55708f5e1ac5b470e
XiaoYulong <xiaoyulong007@gmail.com>
2015-10-25 21:40:10 +0800

    project initial

---
+++ GPUImageDemo/GPUImageDemo/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  GPUImageDemo
+//
+//  Created by 肖 玉龙 on 15/10/25.
+//  Copyright (c) 2015年 xyl. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "GPUImage.h"
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
